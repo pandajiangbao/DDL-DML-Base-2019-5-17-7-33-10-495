d6adadfb13852ee0cbba0c1228703c19a4980e2e
pandajiangbao <37300229+pandajiangbao@users.noreply.github.com>
2019-07-04 23:51:44 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,9 @@
 -- 插入记录  
-
+	insert into student values ('003','王五',23,'男');
 -- 修改记录  
-
+	update student set name = '张三三' where id = '001' ;
 --  删除记录  
+	delete from student where id='001';
 
 -- 查询记录  
+	select * from student  where id='002';
